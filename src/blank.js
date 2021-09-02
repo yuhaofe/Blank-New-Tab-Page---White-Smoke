6c4fbf4631dd51d1b85f62467529e3f01246b860
Yuhao Feng <flyhaozi@live.com>
2021-09-03 04:09:35 +0800

    Add dark mode

--- src/blank.js
+++ src/blank.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.title = chrome.i18n.getMessage("new_tab_page");
+    chrome.storage.onChanged.addListener(loadOptions);
+    loadOptions();
 });
+
+function loadOptions() {
+    chrome.storage.local.get(['theme'], result => {
+        if (!result.theme) {
+            result.theme = 'light';
+            chrome.storage.local.set({ theme: result.theme });
+        }
+        document.body.className = 'theme-' + result.theme;
+    });
+}
--- src/options.js
+++ src/options.js
@@ -3,4 +3,16 @@ document.addEventListener('DOMContentLoaded', function () {
     document.querySelectorAll('[data-locale]').forEach(elem => {
         elem.innerText = chrome.i18n.getMessage(elem.dataset.locale);
     });
+    document.querySelectorAll('input[type="radio"][name="theme"]').forEach(radio => {
+        radio.addEventListener('change', event => {
+            chrome.storage.local.set({ theme: event.target.value });
+        });
+    });
+    chrome.storage.local.get(['theme'], result => {
+        if (!result.theme) {
+            result.theme = 'light';
+            chrome.storage.local.set({ theme: result.theme });
+        }
+        document.querySelector('input[type="radio"][name="theme"][value="' + result.theme + '"]').checked = true;
+    });
 });
