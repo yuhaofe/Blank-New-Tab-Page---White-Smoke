3b880514ba9edd3c84310e6c30ed7b2b7ff4a3d7
Yuhao Feng <git@yuhaofe.com>
2022-01-29 20:15:37 +0800

    Support light/dark in incognito mode

--- src/blank.js
+++ src/blank.js
@@ -1,41 +1,46 @@
 (function() {
     function ThemeManager(theme = 'light') {
         this.theme = theme;
-        this.colors = {
+        this.colorsNormal = {
             light: 'whitesmoke',
-            dark: '#333333',
-            incognito: '#222222'
+            dark: '#333333'
+        };
+        this.colorsIncognito = {
+            light: '#999999',
+            dark: '#222222'
         };
     }
     
     ThemeManager.prototype.loadTheme = async function() {
-        if (chrome.extension.inIncognitoContext) { 
-            this.theme = 'incognito';
-        } else {
-            const lastTheme = localStorage.getItem('lastTheme');
-            if (lastTheme) {
-                this.theme = lastTheme;
-            }
-            let result = await chrome.storage.local.get(['theme']);
-            if (!result.theme) {
-                result.theme = 'light';
-                chrome.storage.local.set({ theme: result.theme });
-            }
-            if (result.theme != lastTheme) {
-                localStorage.setItem('lastTheme', result.theme);
-                this.theme = result.theme;
-                this.apply();
-            }
+        const lastTheme = localStorage.getItem('lastTheme');
+        if (lastTheme) {
+            this.theme = lastTheme;
+        }
+
+        let result = await chrome.storage.local.get(['theme']);
+        if (!result.theme) {
+            result.theme = 'light';
+            chrome.storage.local.set({ theme: result.theme });
+        }
+        if (result.theme != lastTheme) {
+            localStorage.setItem('lastTheme', result.theme);
+            this.theme = result.theme;
+            this.apply();
         }
     }
     
     ThemeManager.prototype.generateStyle = function() {
+        let colors = this.colorsNormal;
+        if (chrome.extension.inIncognitoContext) { 
+            colors = this.colorsIncognito;
+        }
+
         let style = '';
         if (this.theme === 'system') {
-            style += `body { background-color: ${this.colors.light}; }`
-                    + ` @media (prefers-color-scheme: dark) { body { background-color: ${this.colors.dark}; } }`;
+            style += `body { background-color: ${colors.light}; }`
+                    + ` @media (prefers-color-scheme: dark) { body { background-color: ${colors.dark}; } }`;
         } else {
-            style += `body { background-color: ${this.colors[this.theme]}; }`;
+            style += `body { background-color: ${colors[this.theme]}; }`;
         }
         return style;
     }
